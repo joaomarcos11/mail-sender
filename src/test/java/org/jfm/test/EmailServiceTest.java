07af74bd630d4c9520ef3a353d015d12ee75947e
Joao Marcos <joaomarcos11.tech@gmail.com>
2025-02-07 22:19:20 -0300

    teste

--- src/test/java/org/jfm/test/EmailServiceTest.java
+++ src/test/java/org/jfm/test/EmailServiceTest.java
@@ -1,34 +1,49 @@
-// package org.jfm.test;
+package org.jfm.test;
 
-// import org.jfm.application.EmailService;
-// import io.quarkus.mailer.MockMailbox;
-// import io.quarkus.test.junit.QuarkusTest;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import jakarta.inject.Inject;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.jfm.application.EmailService;
 
-// @QuarkusTest
-// public class EmailServiceTest {
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.MockMailbox;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import jakarta.inject.Inject;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//     @Inject
-//     EmailService emailService;
+@QuarkusTest
+public class EmailServiceTest {
 
-//     @Inject
-//     MockMailbox mockMailbox;
+    @Inject
+    EmailService emailService;
 
-//     @BeforeEach
-//     void setUp() {
-//         mockMailbox.clear();
-//     }
+    @Inject
+    MockMailbox mockMailbox;
 
-//     @Test
-//     void testSendEmail() {
-//         String email = "test@example.com";
-//         String messageContent = "Success! Your video is ready!";
+    @BeforeEach
+    void setUp() {
+        mockMailbox.clear();
+    }
 
-//         emailService.sendEmail(email, messageContent);
+    // @Test
+    // void testSendEmail() {
+    //     String email = "test@example.com";
+    //     String messageContent = "Success! Your video is ready!";
 
-//         assertEquals(1, mockMailbox.getMessagesSentTo(email).size());
-//     }
-// }
+    //     emailService.sendEmail(email, messageContent);
+
+    //     assertEquals(1, mockMailbox.getMessagesSentTo(email).size());
+    // }
+
+    // @Test
+    // public void sendTestMail() {
+    //     try {
+    //         LOG.debug("Building mail");
+    //         Mail mail = Mail.withText("recipient@example.com", "Test Subject", "Test email body.");
+    //         LOG.debug("Sending mail via mailer");
+    //         mailer.send(mail);
+    //         LOG.info("Mail sent successfully.");
+    //     } catch (Exception e) {
+    //         LOG.error("Error sending mail", e);
+    //     }
+    // }
+}
